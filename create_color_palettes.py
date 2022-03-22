fbd9c069d483351067ea0a13164ed00f99414b82
Francois_Albouy <francois_albouy@epam.com>
2022-03-22 12:07:38 +0100

    color palette test create a palette from a base color using hsv.

---
+++ create_color_palettes.py
@@ -0,0 +1,39 @@
+from matplotlib import pyplot as plt
+import seaborn as sb
+
+# current_palette = sb.color_palette("mako",15)
+current_palette = sb.cubehelix_palette(start=.5, rot=-.5, dark=0, light=.95, n_colors=15)
+sb.palplot(current_palette)
+#plt.show()
+
+import colorsys
+N = 10
+
+base_color_hsv = colorsys.rgb_to_hsv(0.0,119/255,212/255)
+print(base_color_hsv)
+c = list(base_color_hsv)
+c[1]=1.0
+c[2]=1.0
+base_color_hsv= tuple( c )
+
+HSV_tuples = [(base_color_hsv[0] , base_color_hsv[1]-x*0.1 , base_color_hsv[2]) for x in range(N)]
+RGB_tuples = []
+for c in HSV_tuples:
+    print(c)
+    rgb = map(lambda x: x, colorsys.hsv_to_rgb(*c))
+    RGB_tuples.append(list(rgb))
+
+colors =  RGB_tuples
+print(colors)
+
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+import numpy as np
+
+cmap = ListedColormap(colors)
+
+a = np.outer(np.linspace(0, 1, 20), np.linspace(0, 1, 20))
+im = plt.imshow(a, cmap=cmap)
+plt.colorbar(im)
+plt.show()
+
